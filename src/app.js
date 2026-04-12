a301b4d82b7fedcc6e4c908308bad67766bf77e8
oavqt <81715722+oavqt@users.noreply.github.com>
2026-04-12 05:43:28 -0600

    SIMPLE ROUTING PRACTICE USING EXPRESS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pages = path.join(__dirname, '..', 'public', 'pages');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('IS THIS WORKING');
+  res.sendFile(path.join(pages, '..', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(pages, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(pages, 'contact.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(pages, 'error.html'));
 });
 
 const port = 3333;
